900436499530c57866488bfb103b168022f20d33
Jason Ji <uberjason@gmail.com>
2022-06-20 21:16:43 -0400

    Initial Commit

---
+++ DatePickerWoes_iOS16Beta1/DatePickerWoes_iOS16Beta1App.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerWoes_iOS16Beta1App.swift
+//  DatePickerWoes_iOS16Beta1
+//
+//  Created by Jason Ji on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerWoes_iOS16Beta1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
